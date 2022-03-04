118910b9094fbb4cd58f1e14a3f2cc422a0ba43c
talalek4s <talalek@gmail.com>
2022-03-04 08:41:01 -0600

    Version V2.1.0 Mejorada

--- src/main/java/com/example/clavesui/ContrasenaUI.java
+++ src/main/java/com/example/clavesui/ContrasenaUI.java
@@ -9,11 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
 
 public class ContrasenaUI extends Application {
 
@@ -43,7 +39,7 @@ public class ContrasenaUI extends Application {
         button.setOnAction(e -> {
             int valido = 0;
             do {
-                valido = esNumero(textField.getText());
+                valido = Funciones.esNumero(textField.getText());
                 if (valido == 0){
                     System.out.println("Intente de nuevo ");
                     break;
@@ -54,7 +50,7 @@ public class ContrasenaUI extends Application {
 
                 if (valor >= 8 && valor <=50) {
 
-                    mensaje = KeyGenerator(valor);
+                    mensaje = Funciones.KeyGenerator(valor);
                     textFieldContrasena.setText(mensaje);
                   }
                 else{
@@ -73,100 +69,5 @@ public class ContrasenaUI extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-    String KeyGenerator(int lCadena) {
-
-
-        String clave = "";
-        Contrasena contrasena = new Contrasena("", 13);
-        char[] mayusculas = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        char[] minusculas = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        char[] signo = {'!','"','#','$','%','&', '(',')','*','+', ',','-','.','/', ':',';','<','=','>','?','@', '[',']','^','_', '{','|','}','~'};
-        char[] numero = {'0','1','2','3','4','5','6','7','8','9'};
-
-        int longitudClave;
-        int grupo;
-        int numeroCaracter;
-        String letra;
-       // int respuesta = 0;
-
-/*        do{
-
-            Scanner reader = new Scanner(System.in);
-            System.out.println("Ingrese un entero mayor que 8 y menor que 50");
-            longitudClave = reader.nextInt();
-
-            if(longitudClave >= 8 && longitudClave <= 50) respuesta = 1;
-            else System.out.println("Longitud erronea, vuelva a intentar ");
-
-        }while(respuesta != 1);*/
-
-        longitudClave = lCadena;
-
-        for(int i = 1; i <= longitudClave; i++){
-
-            grupo = fSemilla(4);
-            switch(grupo){
-                case 0 -> {
-                    numeroCaracter = fSemilla(26);
-                    letra = String.valueOf((mayusculas[numeroCaracter]));
-                    clave = clave.concat(letra);
-                }
-                case 1 -> {
-                    numeroCaracter = fSemilla(26);
-                    letra = String.valueOf((minusculas[numeroCaracter]));
-                    clave = clave.concat(letra);
-                }
-                case 2 -> {
-                    numeroCaracter = fSemilla(29);
-                    letra = String.valueOf((signo[numeroCaracter]));
-                    clave = clave.concat(letra);
-                }
-                case 3 -> {
-                    numeroCaracter = fSemilla(10);
-                    letra = String.valueOf((numero[numeroCaracter]));
-                    clave = clave.concat(letra);
-                }
-                default -> {
-                }
-            }//switch
-
-        }//for
-
-        contrasena.setContrasena(clave);
-        System.out.println(contrasena.getContrasena());
-
-        return contrasena.getContrasena();
-    }
-
-    static int fSemilla(int dato){
-
-        Calendar calendario = new GregorianCalendar();
-        int minuto = calendario.get(Calendar.MINUTE);
-        int segundo = calendario.get(Calendar.SECOND);
-
-        int semilla = minuto + segundo+ (int) Math.floor(Math.random()*dato);
-        semilla = semilla %dato;
-
-        return semilla;
-    }
-
-    public  static  int esNumero(String string){
-
-        int intValor;
-
-        if(string == null || string.equals("")){
-            System.out.println("No numero ");
-            return  0;
-        }
-        try{
-            intValor = Integer.parseInt(string);
-            return 1;
-        }catch (NumberFormatException e){
-            System.out.println("No es un numero ");
-            return 0;
-        }
-
-    }
-
+    
 }
